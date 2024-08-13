38517a7c97bc2f098266039a974d431d64cfa538
zccc <825846405@qq.com>
2024-08-13 14:10:07 +0800

    zccc提交

--- git测试/git测试/Program.cs
+++ git测试/git测试/Program.cs
@@ -6,11 +6,7 @@
         {
             Console.WriteLine("hello world");
 
-<<<<<<< HEAD
             Console.WriteLine("zcccc修改了代码");
-=======
-            Console.WriteLine("");
->>>>>>> d10f019c7ca67c08677f9acb4d192305138cdb02
         }
     }
 }
